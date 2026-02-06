e75f25b8a31af490477892e02d3f702d4956418d
huseynalasgarli <huseynlsgrli9@gmail.com>
2026-02-06 21:12:32 +0400

    fifth

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -27,7 +27,12 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
-        Patient patient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+            throw new EmailAlreadyExistsException("A patient with this email already exists" + patientRequestDTO.getEmail());
+        }
+
+        Patient patient = patientRepository.save(
+                PatientMapper.toModel(patientRequestDTO));
         return PatientMapper.toDTO(patient);
     }
 }
