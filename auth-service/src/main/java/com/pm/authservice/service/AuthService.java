23c4f987ea195e3725cf3f9105c167254c64e7e4
huseynalasgarli <huseynlsgrli9@gmail.com>
2026-02-20 05:03:59 +0400

    remove iml

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -5,6 +5,7 @@ import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
 
 import com.pm.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -31,5 +32,14 @@ public class AuthService {
                 .map(u ->jwtUtil.generateToken(u.getEmail(),u.getRole()));
 
          return token;
+    }
+
+    public boolean validateToken(String token) {
+        try{
+            jwtUtil.validateToken(token);
+            return true;
+        }catch (JwtException e){
+            return false;
+        }
     }
 }
--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,12 +1,14 @@
 package com.pm.authservice.util;
 
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
 
@@ -28,4 +30,25 @@ public class JwtUtil {
                 .signWith(secretKey)
                 .compact();
     }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+//        catch (SignatureException e) {
+//            throw new JwtException("Invalid JWT signature");
+        } catch (ExpiredJwtException e) {
+            throw new JwtException("JWT token has expired");
+        } catch (MalformedJwtException e) {
+            throw new JwtException("Malformed JWT token");
+        } catch (UnsupportedJwtException e) {
+            throw new JwtException("Unsupported JWT token");
+        } catch (IllegalArgumentException e) {
+            throw new JwtException("JWT claims string is empty");
+        } catch (JwtException e) {
+            throw new JwtException("Invalid JWT token");
+        }
+    }
 }
+
